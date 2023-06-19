7284a0694dd668ef8ec8487007b40609dbd68b50
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-06-20 00:24:52 +0200

    updated index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './core/App';
 import reportWebVitals from './reportWebVitals';
+import { Normalize } from 'styled-normalize';
+import { GlobalStyles } from './common/GlobalStyled';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './common/theme';
+import { Provider } from 'react-redux';
+import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <React.StrictMode>
+        <GlobalStyles />
+        <Normalize />
+        <App />
+      </React.StrictMode>
+    </ThemeProvider>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
